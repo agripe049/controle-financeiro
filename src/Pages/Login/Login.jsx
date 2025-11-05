8f91de4b58ff572df14fcaffa49ccbf0a0b37e0b
agripe049 <agripeheber@gmail.com>
2025-11-04 22:59:44 -0300

    Autenticação de login pronta, falta fazer as medidas de segurança

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,64 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../services/FirebaseConfig";
 import "./Login.css"
+import { useNavigate } from "react-router-dom";
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true)
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/home')
+    } catch (err) {
+      setError("E-mail ou senha incorretos!");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div><h1>Login</h1></div>
+    <div className="login-container">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label>Senha</label>
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        {error && <p>{error}</p>}
+
+        <button>
+          {loading ? "Entrando..." : "Entrar"}
+        </button>
+      </form>
+    </div>
   )
 }
 
